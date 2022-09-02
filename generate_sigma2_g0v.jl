d8ea49daa1a1ba509302a1f1ffb6b4fa62140a05
dcerkoney <dcerkoney@gmail.com>
2022-09-02 15:34:43 -0400

    Add script to build 2nd-order self-energy diagram

--- generate_sigma2_g0v.jl
+++ generate_sigma2_g0v.jl
@@ -1,9 +1,12 @@
 using FeynmanDiagram
+using AbstractTrees
+using Parameters
+using StaticArrays
 # using Combinatorics: with_replacement_combinations
 
 const DiagramF64 = Diagram{Float64}
 
-function propr_params(type::DiagramType, n_order, firstTauIdx)
+function propr_params(type, n_order, firstTauIdx)
     @assert n_order >= 2
     return DiagParaF64(
         type=type,
@@ -16,25 +19,28 @@ function propr_params(type::DiagramType, n_order, firstTauIdx)
     )
 end
 
+DiagTree.uidreset()
+
 # Expansion order (O(V^n})) for this run
 n_order = 2  # First, generate the bare O(V^2) self-energy
-
-k = vcat([1, 0, 0], repeat([0], n_order - 2)) # external momentum
+    
+k  = vcat([1, 0, 0], repeat([0], n_order - 2)) # external momentum
 k1 = vcat([0, 1, 0], repeat([0], n_order - 2)) # k1 = k + q1
 k3 = vcat([0, 0, 1], repeat([0], n_order - 2)) # k3 = k + q2
-k2 = k1 + k3 - k                    # k2 = k + q1 + q2
+k2 = k1 + k3 - k                               # k2 = k + q1 + q2
 q1 = k1 - k
 q2 = k3 - k
-# legk = [-q, k2]                   # [Q, Kin] for Gamma_3
+# legk = [-q, k2]                              # [Q, Kin] for Gamma_3
 
 # Bare Green's function labels, times, and momenta
-g_names = [[G0_1], [G0_2], [G0_3]]
-g_taus = [[1, 2], [2, 1], [1, 2]]
-g_ks = [k1, k2, k3]
+g_names = [:G0_1, :G0_2, :G0_3]
+g_taus  = [[1, 2], [2, 1], [1, 2]]
+g_ks    = [k1, k2, k3]
 
 # Bare interaction labels and momenta
-v_names = [[V_1], [V_2]]
-v_qs = [q1, q2]
+v_names = [:V_1, :V_2]
+v_taus  = [[1, 1], [2, 2]]
+v_qs    = [q1, q2]
 
 # Bare Green's function params
 g_params = [propr_params(GreenDiag, n_order, g_taus[i][1]) for i in 1:3]
@@ -45,13 +51,12 @@ v_params = [propr_params(Ver4Diag, n_order, i) for i in 1:2]
 # Hard-coded Bare Green's function and interaction lines
 g_lines = [
     Diagram{Float64}(BareGreenId(g_params[i], k=g_ks[i], t=g_taus[i]),
-                     name=g_names[i])
+        name=g_names[i])
     for i in 1:3
 ]
 v_lines = [
     Diagram{Float64}(BareInteractionId(v_params[i], ChargeCharge,
-                                       k=v_qs[i], permu=Di),
-                     name=v_names[i])
+        k=v_qs[i], t=v_taus[i], permu=Di), name=v_names[i])
     for i in 1:2
 ]
 
@@ -65,15 +70,15 @@ sigma2_params = DiagParaF64(
     firstTauIdx=1,
     interaction=[Interaction(ChargeCharge, Instant),],
 )
-id = GenericID(sigma2_params)
+id = GenericId(sigma2_params)
 sigma2 = DiagramF64(id, Prod(), [g_lines; v_lines])
 
 # print the tree
-print_tree(sigma2.diagram)
+print_tree(sigma2)
 
 # plot the tree
 # plot_tree(sigma2)
 
 # build expression tree
-sigma2_exprtree = ExprTree.build(sigma2.diagram)
+sigma2_exprtree = ExprTree.build([sigma2])
 println(sigma2_exprtree)
