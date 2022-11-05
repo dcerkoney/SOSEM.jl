132a45f63f52022c59d47654b5f6a801420f996f
dcerkoney <dcerkoney@gmail.com>
2022-11-05 17:21:44 -0400

    Prune/rename integration examples

--- examples/c1c/integrate_c1c.jl
+++ examples/c1c/integrate_c1c.jl
@@ -1,5 +1,6 @@
 using ElectronGas
-using ElectronLiquid.UEG: ParaMC
+using ElectronLiquid.UEG: ParaMC, short
+using FeynmanDiagram
 using JLD2
 using Measurements
 using SOSEM
@@ -15,16 +16,18 @@ function main()
         ENV["JULIA_DEBUG"] = SOSEM
     end
 
-    # Settings for diagram generation
     settings = DiagGen.Settings(;
         observable=DiagGen.c1c,
-        n_order=2,
+        n_order=4,
         verbosity=DiagGen.quiet,
         expand_bare_interactions=false,
+        filter=[NoHartree],
+        interaction=[FeynmanDiagram.Interaction(ChargeCharge, Instant)],  # Yukawa-type interaction
+        # interaction=[FeynmanDiagram.Interaction(ChargeCharge, Dynamic)],  # TODO: test RPA-type interaction
     )
 
     # UEG parameters for MC integration
-    param = ParaMC(; order=settings.n_order, rs=2.0, beta=200.0, mass2=0.1, isDynamic=false)
+    param = ParaMC(; order=settings.n_order, rs=1.0, beta=200.0, mass2=2.0, isDynamic=false)
     @debug "β * EF = $(param.beta), β = $(param.β), EF = $(param.EF)"
 
     # K-mesh for measurement
@@ -41,21 +44,33 @@ function main()
     # Number of evals below and above kF
     neval = 1e8
 
-    # Generate the diagrams
-    diagparam, diagtree, exprtree = DiagGen.build_nonlocal(settings)
+    # Enable/disable interaction and chemical potential counterterms
+    renorm_mu = true
+    renorm_lambda = true
+
+    # Build diagram and expression trees for all loop and counterterm partitions
+    partitions, diagparams, diagtrees, exprtrees = DiagGen.build_nonlocal_with_ct(
+        settings;
+        fixed_order=false,
+        renorm_mu=renorm_mu,
+        renorm_lambda=renorm_lambda,
+    )
+
+    println("Integrating partitions: $partitions")
+    @debug "diagtrees: $diagtrees"
+    @debug "exprtrees: $exprtrees"
 
     # Check the diagram tree
-    # DiagGen.checktree(diagtree, settings)
-
-    # NOTE: We assume there is only a single root in the ExpressionTree
-    @assert length(exprtree.root) == 1
+    # for d in diagtrees
+    #     DiagGen.checktree(d, settings)
+    # end
 
     # Bin external momenta, performing a single integration
-    res = UEG_MC.integrate_nonlocal(
+    res = UEG_MC.integrate_nonlocal_with_ct(
         settings,
         param,
-        diagparam,
-        exprtree;
+        diagparams,
+        exprtrees;
         kgrid=kgrid,
         alpha=alpha,
         neval=neval,
@@ -69,12 +84,21 @@ function main()
         intn_str = "no_bare_"
     end
 
+    # Distinguish results with different counterterm schemes
+    ct_string = (renorm_mu || renorm_lambda) ? "with_ct" : ""
+    if renorm_mu
+        ct_string *= "_mu"
+    end
+    if renorm_lambda
+        ct_string *= "_lambda"
+    end
+
     # Save to JLD2 on main thread
     if !isnothing(res)
         savename =
             "results/data/c1c_n=$(param.order)_rs=$(param.rs)_" *
             "beta_ef=$(param.beta)_lambda=$(param.mass2)_" *
-            "neval=$(neval)_$(intn_str)$(solver)"
+            "neval=$(neval)_$(intn_str)$(solver)_$(ct_string)"
         jldopen("$savename.jld2", "a+") do f
             key = "$(short(param))"
             if haskey(f, key)
@@ -87,12 +111,10 @@ function main()
         if plot
             # Plot the result
             fig, ax = plt.subplots()
+            # Compare with bare quadrature results (stored in Hartree a.u.)
             if compare_bare
-                # Compare with bare quadrature results (stored in Hartree a.u.);
-                # since the bare result is independent of rs after non-dimensionalization, we
-                # are free to mix rs of the current MC calculation with this result at rs = 2.
-                # Similarly, the bare results were calculated at zero temperature (beta is arb.)
-                rs_quad = 2.0
+                # NOTE: The bare results were calculated at zero temperature (beta is arb.)
+                rs_quad = 1.0
                 sosem_quad = np.load("results/data/soms_rs=$(rs_quad)_beta_ef=200.0.npz")
                 # np.load("results/data/soms_rs=$(Float64(param.rs))_beta_ef=$(param.beta).npz")
                 k_kf_grid_quad = np.linspace(0.0, 3.0; num=600)
@@ -104,33 +126,49 @@ function main()
                     k_kf_grid_quad,
                     c1c_quad_dimless,
                     "k";
-                    label="\$n=$(param.order)\$ (quad)",
+                    label="\$\\mathcal{P}=$((2,0,0))\$ (quad)",
                 )
             end
-            ax.plot(
-                k_kf_grid,
-                means,
-                "o-";
-                markersize=2,
-                color="C0",
-                label="\$n=$(param.order)\$ ($solver)",
-            )
-            ax.fill_between(
-                k_kf_grid,
-                means - stdevs,
-                means + stdevs;
-                color="C0",
-                alpha=0.4,
-            )
-            ax.legend(; loc="best")
+            for o in eachindex(partitions)
+                # Get means and error bars from the result for this partition
+                local means, stdevs
+                if res.config.N == 1
+                    # res gets automatically flattened for a single-partition measurement
+                    means, stdevs = res.mean, res.stdev
+                else
+                    means, stdevs = res.mean[o], res.stdev[o]
+                end
+                # Data gets noisy above 1st Green's function counterterm order
+                marker = partitions[o][2] > 1 ? "o-" : "-"
+                # Data gets noisy above 3rd loop order
+                # marker = partitions[o][1] > 3 ? "o-" : "-"
+                ax.plot(
+                    k_kf_grid,
+                    means,
+                    marker;
+                    markersize=2,
+                    color="C$(o - 1)",
+                    label="\$\\mathcal{P}=$(partitions[o])\$ ($solver)",
+                )
+                ax.fill_between(
+                    k_kf_grid,
+                    means - stdevs,
+                    means + stdevs;
+                    color="C$(o - 1)",
+                    alpha=0.4,
+                )
+            end
+            ax.legend(; loc="lower right")
             ax.set_xlabel("\$k / k_F\$")
-            ax.set_ylabel("\$C^{(1c)}(\\mathbf{k}) \\,/\\, E^{2}_{\\mathrm{TF}}\$")
+            ax.set_ylabel(
+                "\$C^{(1c)}_{\\mathcal{P}}(\\mathbf{k}) \\,/\\, E^{2}_{\\mathrm{TF}}\$",
+            )
             ax.set_xlim(minimum(k_kf_grid), maximum(k_kf_grid))
             plt.tight_layout()
             fig.savefig(
                 "results/c1c/n=$(param.order)/c1c_n=$(param.order)_rs=$(param.rs)_" *
                 "beta_ef=$(param.beta)_lambda=$(param.mass2)_" *
-                "neval=$(neval)_$(intn_str)$(solver).pdf",
+                "neval=$(neval)_$(intn_str)$(solver)_$(ct_string).pdf",
             )
             plt.close("all")
         end
--- examples/c1d/integrate_c1d.jl
+++ examples/c1d/integrate_c1d.jl
@@ -1,5 +1,6 @@
 using ElectronGas
-using ElectronLiquid.UEG: ParaMC
+using ElectronLiquid.UEG: ParaMC, short
+using FeynmanDiagram
 using JLD2
 using Measurements
 using SOSEM
@@ -15,51 +16,70 @@ function main()
         ENV["JULIA_DEBUG"] = SOSEM
     end
 
-    # Settings for diagram generation
     settings = DiagGen.Settings(;
         observable=DiagGen.c1d,
-        n_order=2,
-        verbosity=DiagGen.info,
+        n_order=3,
+        verbosity=DiagGen.quiet,
         expand_bare_interactions=false,
+        filter=[NoHartree],
+        interaction=[FeynmanDiagram.Interaction(ChargeCharge, Instant)],  # Yukawa-type interaction
+        # interaction=[FeynmanDiagram.Interaction(ChargeCharge, Dynamic)],  # TODO: test RPA-type interaction
     )
 
+    # TODO: Write special-purpose integrator for bare result; currently using the old numerically
+    #       exact results from quadrature, wich are valid for `expand_bare_interactions=false` only!
+    @assert settings.expand_bare_interactions == false
+
     # UEG parameters for MC integration
-    param = ParaMC(; order=settings.n_order, rs=0.5, isDynamic=false, beta=600.0, mass2=0.1)
-    @debug "β * EF = $(param.beta), β = $(param.β), EF = $(param.EF)" maxlog = 1
+    param = ParaMC(; order=settings.n_order, rs=1.0, beta=200.0, mass2=2.0, isDynamic=false)
+    @debug "β * EF = $(param.beta), β = $(param.β), EF = $(param.EF)"
 
     # K-mesh for measurement
     k_kf_grid = np.load("results/kgrids/kgrid_vegas_dimless_n=77_small.npy")
     kgrid = param.kF * k_kf_grid
 
     # Settings
-    alpha = 2.5
+    alpha = 2.0
     print = 0
     plot = true
     compare_bare = true
     solver = :vegasmc
 
     # Number of evals below and above kF
-    neval = 1e6
+    neval = 5e8
 
-    # Generate the diagrams
-    diagparam, diagtree, exprtree = DiagGen.build_nonlocal(settings)
+    # Enable/disable interaction and chemical potential counterterms
+    renorm_mu = true
+    renorm_lambda = true
+
+    # Build diagram and expression trees for all loop and counterterm partitions
+    partitions, diagparams, diagtrees, exprtrees = DiagGen.build_nonlocal_with_ct(
+        settings;
+        fixed_order=true,  # TODO: special-purpose integrator for (2,0,0) partition
+        renorm_mu=renorm_mu,
+        renorm_lambda=renorm_lambda,
+    )
+
+    println("Integrating partitions: $partitions")
+    @debug "diagtrees: $diagtrees"
+    @debug "exprtrees: $exprtrees"
 
     # Check the diagram tree
-    DiagGen.checktree(diagtree, settings)
-
-    # NOTE: We assume there is only a single root in the ExpressionTree
-    @assert length(exprtree.root) == 1
+    # for d in diagtrees
+    #     DiagGen.checktree(d, settings)
+    # end
 
     # Bin external momenta, performing a single integration
-    res = UEG_MC.integrate_nonlocal(
+    res = UEG_MC.integrate_nonlocal_with_ct(
         settings,
         param,
-        diagparam,
-        exprtree;
+        diagparams,
+        exprtrees;
         kgrid=kgrid,
         alpha=alpha,
         neval=neval,
         print=print,
+        solver=solver,
     )
 
     # Distinguish results with fixed vs re-expanded bare interactions
@@ -68,12 +88,21 @@ function main()
         intn_str = "no_bare_"
     end
 
+    # Distinguish results with different counterterm schemes
+    ct_string = (renorm_mu || renorm_lambda) ? "with_ct" : ""
+    if renorm_mu
+        ct_string *= "_mu"
+    end
+    if renorm_lambda
+        ct_string *= "_lambda"
+    end
+
     # Save to JLD2 on main thread
     if !isnothing(res)
         savename =
             "results/data/c1d_n=$(param.order)_rs=$(param.rs)_" *
             "beta_ef=$(param.beta)_lambda=$(param.mass2)_" *
-            "neval=$(neval)_$(intn_str)$(solver)"
+            "neval=$(neval)_$(intn_str)$(solver)_$(ct_string)"
         jldopen("$savename.jld2", "a+") do f
             key = "$(short(param))"
             if haskey(f, key)
@@ -86,12 +115,10 @@ function main()
         if plot
             # Plot the result
             fig, ax = plt.subplots()
+            # Compare with bare quadrature results (stored in Hartree a.u.)
             if compare_bare
-                # Compare with bare quadrature results (stored in Hartree a.u.);
-                # since the bare result is independent of rs after non-dimensionalization, we
-                # are free to mix rs of the current MC calculation with this result at rs = 2.
-                # Similarly, the bare results were calculated at zero temperature (beta is arb.)
-                rs_quad = 2.0
+                # NOTE: The bare results were calculated at zero temperature (beta is arb.)
+                rs_quad = 1.0
                 sosem_quad = np.load("results/data/soms_rs=$(rs_quad)_beta_ef=200.0.npz")
                 # np.load("results/data/soms_rs=$(Float64(param.rs))_beta_ef=$(param.beta).npz")
                 k_kf_grid_quad = np.linspace(0.0, 3.0; num=600)
@@ -103,33 +130,49 @@ function main()
                     k_kf_grid_quad,
                     c1d_quad_dimless,
                     "k";
-                    label="\$n=$(param.order)\$ (quad)",
+                    label="\$\\mathcal{P}=$((2,0,0))\$ (quad)",
                 )
             end
-            ax.plot(
-                k_kf_grid,
-                means,
-                "o-";
-                markersize=2,
-                color="C0",
-                label="\$n=$(param.order)\$ ($solver)",
-            )
-            ax.fill_between(
-                k_kf_grid,
-                means - stdevs,
-                means + stdevs;
-                color="C0",
-                alpha=0.4,
-            )
-            ax.legend(; loc="best")
+            for o in eachindex(partitions)
+                # Get means and error bars from the result for this partition
+                local means, stdevs
+                if res.config.N == 1
+                    # res gets automatically flattened for a single-partition measurement
+                    means, stdevs = res.mean, res.stdev
+                else
+                    means, stdevs = res.mean[o], res.stdev[o]
+                end
+                # Data gets noisy above 1st Green's function counterterm order
+                marker = partitions[o][2] > 1 ? "o-" : "-"
+                # Data gets noisy above 3rd loop order
+                # marker = partitions[o][1] > 3 ? "o-" : "-"
+                ax.plot(
+                    k_kf_grid,
+                    means,
+                    marker;
+                    markersize=2,
+                    color="C$(o - 1)",
+                    label="\$\\mathcal{P}=$(partitions[o])\$ ($solver)",
+                )
+                ax.fill_between(
+                    k_kf_grid,
+                    means - stdevs,
+                    means + stdevs;
+                    color="C$(o - 1)",
+                    alpha=0.4,
+                )
+            end
+            ax.legend(; loc="lower right")
             ax.set_xlabel("\$k / k_F\$")
-            ax.set_ylabel("\$C^{(1d)}(\\mathbf{k}) \\,/\\, q^{4}_{\\mathrm{TF}}\$")
+            ax.set_ylabel(
+                "\$C^{(1d)}_{\\mathcal{P}}(\\mathbf{k}) \\,/\\, E^{2}_{\\mathrm{TF}}\$",
+            )
             ax.set_xlim(minimum(k_kf_grid), maximum(k_kf_grid))
             plt.tight_layout()
             fig.savefig(
                 "results/c1d/n=$(param.order)/c1d_n=$(param.order)_rs=$(param.rs)_" *
                 "beta_ef=$(param.beta)_lambda=$(param.mass2)_" *
-                "neval=$(maxeval)_$(intn_str)$(solver).pdf",
+                "neval=$(neval)_$(intn_str)$(solver)_$(ct_string).pdf",
             )
             plt.close("all")
         end
