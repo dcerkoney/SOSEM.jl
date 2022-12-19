44ecaa784a85adf07c995e6a3d70349f9fbb4cf0
Daniel Cerkoney <dcerkoney@gmail.com>
2022-12-19 00:50:25 -0500

    Update results with counterterm grid-search data

--- examples/counterterms/counterterms_from_sigma.jl
+++ examples/counterterms/counterterms_from_sigma.jl
@@ -2,14 +2,25 @@ using ElectronLiquid
 using JLD2
 
 # Change to counterterm directory
-cd("$(ENV["SOSEM_HOME"])/examples/counterterms")
+cd("$(ENV["SOSEM_CEPH"])/examples/counterterms")
 
 # Physical params matching data for SOSEM observables
-order = [2]  # C^{(1)}_{N≤4} includes CTs up to 2nd order
-rs = [1.0]
-# mass2 = [0.1]
-mass2 = [2.0]
-beta = [20.0]
+order = [3]  # C^{(1)}_{N≤4} includes CTs up to 2nd order
+
+# Grid-search 1: rs, mass2
+rs = LinRange(0.1, 2.0, 5)
+mass2 = LinRange(1.0, 5.0, 5)
+beta = [40.0]
+
+# Grid-search 2: rs, beta
+#rs = LinRange(0.1, 2.0, 5)
+#mass2 = [<lambda_opt>]
+#beta = [20.0, 40.0, 100.0]
+
+# Post-search: finer, wider rs mesh at chosen beta and mass2
+#rs = LinRange(0.1, 3.0, 21)
+#mass2 = [<lambda_opt>]
+#beta = [<beta_opt>]
 
 # Total number of MCMC evaluations
 neval = 1e10
