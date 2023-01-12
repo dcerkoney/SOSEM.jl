090f1201fc4e4a78c7bea631430703e4a2904018
Daniel Cerkoney <dcerkoney@gmail.com>
2023-01-12 15:40:02 -0500

    Update lambda
    Former-commit-id: 9ca41f9c21d677bae8642a94d7a600aa07f3034a

--- examples/c1b0/integrate_c1bL0.jl
+++ examples/c1b0/integrate_c1bL0.jl
@@ -36,7 +36,7 @@ function main()
 
     # UEG parameters for MC integration
     param =
-        ParaMC(; order=settings.max_order, rs=1.0, beta=40.0, mass2=1.0, isDynamic=false)
+        ParaMC(; order=settings.max_order, rs=1.0, beta=40.0, mass2=1.5, isDynamic=false)
     @debug "β * EF = $(param.beta), β = $(param.β), EF = $(param.EF)"
 
     # K-mesh for measurement
