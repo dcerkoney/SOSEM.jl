8c57c513e3ed05b1e6ae34f98039804ee0498cc1
Daniel Cerkoney <dcerkoney@gmail.com>
2023-01-27 14:26:51 -0500

    Update CT scripts
    Former-commit-id: 2f7f2e624e5997f0c94114cc256fd65675256243

--- examples/counterterms/counterterms_from_sigma.jl
+++ examples/counterterms/counterterms_from_sigma.jl
@@ -5,13 +5,13 @@ using JLD2
 cd("$(ENV["SOSEM_CEPH"])/examples/counterterms")
 
 # Physical params matching data for SOSEM observables
-order = [3]  # C^{(1)}_{N≤5} includes CTs up to 3rd order
+order = [4]  # C^{(1)}_{N≤5} includes CTs up to 3rd order
 
 # Grid-search 1: rs, mass2
 # rs = LinRange(0.1, 2.0, 5)
 rs = [1.0]
 #mass2 = LinRange(1.0, 5.0, 5)
-mass2 = [1.5]
+mass2 = [1.0]
 beta = [40.0]
 
 # Grid-search 2: rs, beta
