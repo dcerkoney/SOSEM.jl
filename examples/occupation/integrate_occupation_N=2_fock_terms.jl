acf5bc58606691aaff5f78ad10c85847a80e6cc2
dcerkoney <dcerkoney@gmail.com>
2023-02-14 18:46:02 -0500

    Add benchmark for N=2 occupation Fock insertions
    Former-commit-id: 1048aa884c9f8ab6913b4b842dd0ab048923cbfd

---
+++ examples/occupation/integrate_occupation_N=2_fock_terms.jl
@@ -0,0 +1,353 @@
+using AbstractTrees
+using CodecZlib
+using CompositeGrids
+using ElectronGas
+using ElectronLiquid
+using ElectronLiquid.UEG
+using FeynmanDiagram
+using JLD2
+using Measurements
+using MCIntegration
+using Lehmann
+using LinearAlgebra
+using Parameters
+using PyCall
+using SOSEM
+
+# For saving/loading numpy data
+@pyimport numpy as np
+
+function integrate_occupation(
+    mcparam::UEG.ParaMC,
+    diagparam::DiagParaF64,
+    exprtree::ExprTreeF64;
+    kgrid=[0.0],
+    alpha=3.0,
+    neval=1e5,
+    print=-1,
+    solver=:vegasmc,
+)
+    # We assume that the expression tree has a single root
+    @assert length(exprtree.root) == 1
+
+    # List of expression tree roots, external times, and inner
+    # loop numbers for each tree (to be passed to integrand)
+    innerLoopNum = diagparam.innerLoopNum
+
+    # Grid size
+    n_kgrid = length(kgrid)
+
+    # Temporary array for combined K-variables [ExtK, K].
+    # We use the maximum necessary loop basis size for K pool.
+    varK = zeros(3, diagparam.totalLoopNum)
+
+    # Build adaptable MC integration variables
+    (K, T, ExtKidx) = occupation_mc_variables(mcparam, n_kgrid, alpha)
+
+    # MC configuration degrees of freedom (DOF): shape(K), shape(T), shape(ExtKidx)
+    # We do not integrate the external times and nₖ is instantaneous, hence n_τ = totalTauNum - 1
+    dof = [[diagparam.innerLoopNum, diagparam.totalTauNum - 1, 1]]
+    println("Integration DOF: $dof")
+
+    # UEG SOSEM diagram observables are a function of |k| only (equal-time)
+    obs = [zeros(n_kgrid)]
+
+    # External times are fixed for left/right measurement of the discontinuity at τ = 0
+    T.data[1] = 0  # τin = 0 (= τout⁺)
+
+    # Phase-space factor
+    phase_factor = 1.0 / (2π)^(mcparam.dim * innerLoopNum)
+
+    # Total prefactor; extra minus sign relative to N&O convention for G₀
+    prefactor = -phase_factor
+
+    return integrate(
+        integrand;
+        solver=solver,
+        measure=measure,
+        neval=neval,
+        print=print,
+        # MC config kwargs
+        userdata=(mcparam, exprtree, innerLoopNum, prefactor, varK, kgrid),
+        var=(K, T, ExtKidx),
+        dof=dof,
+        obs=obs,
+    )
+end
+
+"""Build variable pools for the occupation."""
+function occupation_mc_variables(mcparam::UEG.ParaMC, n_kgrid::Int, alpha::Float64)
+    R = Continuous(0.0, 1.0; alpha=alpha)
+    Theta = Continuous(0.0, 1π; alpha=alpha)
+    Phi = Continuous(0.0, 2π; alpha=alpha)
+    K = CompositeVar(R, Theta, Phi)
+    # Offset T pool by 1 for fixed external times (instantaneous Green's function ⟹ τin = τout⁺)
+    T = Continuous(0.0, mcparam.β; offset=1, alpha=alpha)
+    # Bin in external momentum
+    ExtKidx = Discrete(1, n_kgrid; alpha=alpha)
+    return (K, T, ExtKidx)
+end
+
+"""Measurement for multiple diagram trees (counterterm partitions)."""
+function measure(vars, obs, weights, config)
+    ik = vars[3][1]  # ExtK bin index
+    obs[1][ik] += weights[1]
+    return
+end
+
+"""Integrand for the occupation number nₖ."""
+function integrand(vars, config)
+    # We sample internal momentum/times, and external momentum index
+    K, T, ExtKidx = vars
+    R, Theta, Phi = K
+
+    # Unpack userdata
+    mcparam, exprtree, innerLoopNum, prefactor, varK, kgrid = config.userdata
+
+    # External momentum via random index into kgrid (wlog, we place it along the x-axis)
+    ik = ExtKidx[1]
+    varK[1, 1] = kgrid[ik]
+
+    phifactor = 1.0
+    for j in 1:innerLoopNum  # config.dof[i][1]
+        r = R[j] / (1 - R[j])
+        θ = Theta[j]
+        ϕ = Phi[j]
+        varK[1, j + 1] = r * sin(θ) * cos(ϕ)
+        varK[2, j + 1] = r * sin(θ) * sin(ϕ)
+        varK[3, j + 1] = r * cos(θ)
+        phifactor *= r^2 * sin(θ) / (1 - R[j])^2
+    end
+    # @assert T.data[1] == 0
+
+    @debug "K = $(varK)" maxlog = 3
+    @debug "ik = $ik" maxlog = 3
+    @debug "ExtK = $(kgrid[ik])" maxlog = 3
+
+    # Evaluate the expression tree (additional = mcparam)
+    ExprTree.evalKT!(exprtree, varK, T.data, mcparam; eval=UEG_MC.Propagators.eval)
+
+    # Evaluate the occupation number integrand nₖ
+    root = exprtree.root[1]
+    weight = exprtree.node.current
+    return phifactor * prefactor * weight[root]
+end
+
+"""
+Constructs diagram parameters for the Fock self-energy Σ^λ_F(k).
+Since the Fock self-energy is instantaneous, the tau labels do not matter.
+"""
+function fock_param(firstLoopIdx, totalLoopNum)
+    # Instantaneous bare interaction (interactionTauNum = 1) 
+    # => innerLoopNum = totalTauNum = 1
+    return DiagParaF64(;
+        type=SigmaDiag,
+        hasTau=false,
+        firstTauIdx=1,
+        innerLoopNum=1,
+        firstLoopIdx=firstLoopIdx,
+        totalLoopNum=totalLoopNum,
+        totalTauNum=1,
+        filter=[NoHartree],
+        interaction=[FeynmanDiagram.Interaction(ChargeCharge, Instant)],
+    )
+end
+
+"""Builds a Fock self-energy diagram tree."""
+function build_fock(extK; firstLoopIdx, totalLoopNum)
+    return Parquet.sigma(fock_param(firstLoopIdx, totalLoopNum), extK; name=:Σx).diagram[1]
+end
+
+"""f''(ξₖ) (Σ^λ_F(k))^2 ((2,0,0) partition)"""
+function build_term1(print=false, plot=false)
+    n_loop = 2  # p[1]
+    diagparam = DiagParaF64(;
+        type=GreenDiag,
+        innerLoopNum=n_loop,
+        firstTauIdx=4,
+        totalTauNum=4,
+        hasTau=true,
+    )
+    g_param = DiagParaF64(;
+        type=GreenDiag,
+        innerLoopNum=0,
+        firstTauIdx=4,
+        totalLoopNum=diagparam.totalLoopNum,
+        hasTau=true,
+    )
+
+    # Loop basis vector for external momentum
+    k = DiagTree.getK(diagparam.totalLoopNum, 1)
+
+    # Subdiagrams
+    g1 = Parquet.green(g_param, k, (1, 2))
+    g2 = Parquet.green(g_param, k, (2, 3))
+    g3 = Parquet.green(g_param, k, (3, 1))
+    sigmaF1 = build_fock(k; firstLoopIdx=2, totalLoopNum=3)
+    sigmaF2 = build_fock(k; firstLoopIdx=3, totalLoopNum=3)
+
+    # Build term 1
+    extT = (1, 1)
+    id = DiagTree.GreenId(diagparam, Instant; k=k, t=extT)
+    diagtree = DiagramF64(id, Prod(), [g1, sigmaF1, g2, sigmaF2, g3])
+
+    print && print_tree(diagtree)
+    plot && plot_tree(diagtree)
+
+    # Compile to expression tree
+    exprtree = ExprTree.build([diagtree])
+    print && println(exprtree)
+    return diagparam, exprtree
+end
+
+"""-2 f''(ξₖ) Σ^λ_F(k) Σ^λ_F(k_F) ((1,1,0) partition without lambda derivative of Σ_F)"""
+function build_term2(print=false, plot=false)
+    n_loop = 1  # p[1]
+    diagparam = DiagParaF64(;
+        type=GreenDiag,
+        innerLoopNum=n_loop,
+        firstTauIdx=3,
+        totalTauNum=3,
+        hasTau=true,
+    )
+    g_param = DiagParaF64(;
+        type=GreenDiag,
+        innerLoopNum=0,
+        totalLoopNum=diagparam.totalLoopNum,
+        firstTauIdx=3,
+        hasTau=true,
+    )
+
+    # Loop basis vector for external momentum
+    k = DiagTree.getK(diagparam.totalLoopNum, 1)
+
+    # Subdiagrams
+    sigmaF = build_fock(k; firstLoopIdx=2, totalLoopNum=2)
+    g1 = Parquet.green(g_param, k, (1, 2))
+    g2 = Parquet.green(g_param, k, (2, 1))
+    dμ_g2 = DiagTree.derivative([g2], BareGreenId, 1; index=1)[1]
+
+    # Build term 2
+    extT = (1, 1)
+    id = DiagTree.GreenId(diagparam, Instant, [1, 0, 0, 0]; k=k, t=extT)
+    diagtree = DiagramF64(id, Prod(), [g1, sigmaF, dμ_g2]; factor=2.0)
+
+    print && print_tree(diagtree)
+    plot && plot_tree(diagtree)
+
+    # Compile to expression tree
+    exprtree = ExprTree.build([diagtree])
+    print && println(exprtree)
+    return diagparam, exprtree
+end
+
+"""f''(ξₖ) (Σ^λ_F(k_F))^2 ((0,2,0) partition). Note that there is nothing to integrate here."""
+function build_term3(print=false, plot=false)
+    n_loop = 0  # p[1]
+    diagparam = DiagParaF64(;
+        type=GreenDiag,
+        innerLoopNum=n_loop,
+        firstTauIdx=2,
+        totalTauNum=2,
+        hasTau=true,
+    )
+
+    # Loop basis vector for external momentum
+    k = DiagTree.getK(diagparam.totalLoopNum, 1)
+
+    # Build term 3
+    g = Parquet.green(diagparam, k, (1, 1))
+    diagtree = DiagTree.derivative([g], BareGreenId, 2; index=1)[1]
+
+    print && print_tree(diagtree)
+    plot && plot_tree(diagtree)
+
+    # Compile to expression tree
+    exprtree = ExprTree.build([diagtree])
+    print && println(exprtree)
+    return diagparam, exprtree
+end
+
+"""MC integration of the occupation number."""
+function main()
+    # Change to project directory
+    if haskey(ENV, "SOSEM_CEPH")
+        cd(ENV["SOSEM_CEPH"])
+    elseif haskey(ENV, "SOSEM_HOME")
+        cd(ENV["SOSEM_HOME"])
+    end
+
+    # Debug mode
+    if isinteractive()
+        ENV["JULIA_DEBUG"] = Main
+    end
+
+    # UEG parameters for MC integration
+    param = ParaMC(; order=2, rs=1.0, beta=40.0, mass2=1.0, isDynamic=false)
+    @debug "β * EF = $(param.beta), β = $(param.β), EF = $(param.EF)"
+
+    # K-mesh for measurement
+    minK = 0.075 * param.kF
+    Nk, korder = 3, 5
+    kleft =
+        CompositeGrid.LogDensedGrid(
+            :uniform,
+            [0.75 * param.kF, param.kF - 1e-8],
+            [param.kF - 1e-8],
+            Nk,
+            minK,
+            korder,
+        ).grid
+    kright =
+        CompositeGrid.LogDensedGrid(
+            :uniform,
+            [param.kF + 1e-8, 1.25 * param.kF],
+            [param.kF + 1e-8],
+            Nk,
+            minK,
+            korder,
+        ).grid
+    kgrid = [kleft; kright]
+
+    # Dimensionless k-grid
+    k_kf_grid = kgrid / param.kF
+
+    # Settings
+    alpha = 3.0
+    print = 0
+    solver = :vegasmc
+
+    # Number of evals below and above kF
+    neval = 1e9
+
+    # Integrate each term in the N=2 Fock series benchmark
+    second_order_fock_terms = [build_term1(), build_term2(), build_term3()]
+    for (i, (diagparam, exprtree)) in enumerate(second_order_fock_terms)
+        println("Integrating term $i...")
+        res = integrate_occupation(
+            param,
+            diagparam,
+            exprtree;
+            kgrid=kgrid,
+            alpha=alpha,
+            neval=neval,
+            print=print,
+            solver=solver,
+        )
+        println("Done!")
+        # Save to JLD2 on main thread
+        if !isnothing(res)
+            savename = "results/data/occupation_N=2_fock_term_$i"
+            jldopen("$savename.jld2", "a+"; compress=true) do f
+                key = "$(UEG.short(param))"
+                if haskey(f, key)
+                    @warn("replacing existing data for $key")
+                    delete!(f, key)
+                end
+                return f[key] = (param, kgrid, res)
+            end
+        end
+    end
+end
+
+main()
---
+++ examples/occupation/plot_occupation_second_order_fock.jl
@@ -0,0 +1,275 @@
+using CodecZlib
+using ElectronLiquid
+using Interpolations
+using JLD2
+using Lehmann
+using MCIntegration
+using Measurements
+using Parameters
+using PyCall
+using SOSEM
+
+# For saving/loading numpy data
+@pyimport matplotlib.pyplot as plt
+@pyimport mpl_toolkits.axes_grid1.inset_locator as il
+
+"""
+Computes the exact value for the Yukawa-screened Fock self-energy Σ^λ_F(k).
+"""
+function sigma_lambda_fock_exact(k::Float64, param::UEG.ParaMC)
+    # Dimensionless wavenumber at the Fermi surface (x = k / kF)
+    x = k / param.kF
+    # Dimensionless Yukawa mass squared (lambda = λ / kF²)
+    lambda = param.mass2 / param.kF^2
+    # Dimensionless screened Lindhard function
+    F_x_lambda = UEG_MC.screened_lindhard(x; lambda=lambda)
+    # δμ₁ cancels the real part of the Fock self-energy
+    # at the Fermi surface for a Yukawa-screened UEG.
+    return -(param.e0^2 * param.kF / (2 * pi^2 * param.ϵ0)) * F_x_lambda
+end
+
+function main()
+    # Change to project directory
+    if haskey(ENV, "SOSEM_CEPH")
+        cd(ENV["SOSEM_CEPH"])
+    elseif haskey(ENV, "SOSEM_HOME")
+        cd(ENV["SOSEM_HOME"])
+    end
+
+    rs = 1.0
+    beta = 40.0
+    mass2 = 1.0
+    solver = :vegasmc
+
+    # Number of evals
+    neval = 1e7
+
+    # # UEG parameters
+    # max_order = 3
+    # loadparam = ParaMC(; order=max_order, rs=rs, beta=beta, mass2=mass2, isDynamic=false)
+
+    # # Load the raw data
+    # savename =
+    #     "results/data/occupation_n=$(max_order)_rs=$(rs)_" *
+    #     "beta_ef=$(beta)_lambda=$(mass2)_neval=$(neval)_$(solver)"
+    # orders, param, kgrid, partitions, res = jldopen("$savename.jld2", "a+") do f
+    #     key = "$(UEG.short(loadparam))"
+    #     return f[key]
+    # end
+
+    res_list = Result[]
+    local param, kgrid
+    loadparam = ParaMC(; order=2, rs=rs, beta=beta, mass2=mass2, isDynamic=false)
+    for i in 1:3
+        savename = "results/data/occupation_N=2_fock_term_$i"
+        param, kgrid, res = jldopen("$savename.jld2", "a+") do f
+            key = "$(UEG.short(loadparam))"
+            return f[key]
+        end
+        push!(res_list, res)
+    end
+    println(res_list)
+
+    # Get dimensionless k-grid (k / kF) and index corresponding to the Fermi energy
+    k_kf_grid = kgrid / param.kF
+    println(k_kf_grid)
+
+    # Convert results to a Dict of measurements at each order with interaction counterterms merged
+    partitions_list = [[(2, 0, 0)], [(1, 1, 0)], [(0, 2, 0)]]
+    data = UEG_MC.restodict(res_list, partitions_list)
+    println(data)
+
+    # Reexpand merged data in powers of μ
+    z, μ = UEG_MC.load_z_mu(param)
+    δz, δμ = CounterTerm.sigmaCT(2, μ, z; verbose=1)
+    # Test manual renormalization with exact lowest-order chemical potential
+    δμ1 = UEG_MC.delta_mu1(param)  # = ReΣ₁[λ](kF, 0)
+
+    println(UEG.paraid(param))
+
+    # Use LaTex fonts for plots
+    plt.rc("text"; usetex=true)
+    plt.rc("font"; family="serif")
+
+    # Bare occupation fₖ on dense grid for plotting
+    kgrid_fine = param.kF * LinRange(0.75, 1.25, 500)
+    k_kf_grid_fine = LinRange(0.75, 1.25, 500)
+    ϵk_fine = @. kgrid_fine^2 / (2 * param.me) - param.μ
+
+    # Fermi function
+    fe_fine = -Spectral.kernelFermiT.(-1e-8, ϵk_fine, param.β)
+    # First derivative of the Fermi function f'(ϵₖ)
+    fpe_fine = -Spectral.kernelFermiT_dω.(-1e-8, ϵk_fine, param.β)
+    # Second derivative of the Fermi function f''(ϵₖ)
+    fppe_fine = -Spectral.kernelFermiT_dω2.(-1e-8, ϵk_fine, param.β)
+    # Third derivative of the Fermi function f'''(ϵₖ)
+    fpppe_fine = -Spectral.kernelFermiT_dω3.(-1e-8, ϵk_fine, param.β)
+
+    fppe_exact =
+        @. (param.β^2 / 4) * sech(param.β * ϵk_fine / 2)^2 * tanh.(param.β * ϵk_fine / 2)
+    abs_errs = @. abs((fppe_fine - fppe_exact) / fppe_fine)
+    worst_abs_err = maximum(abs_errs)
+    println(
+        "Worst absolute percent error (exact vs. computed f''(ϵₖ)): $(100 * worst_abs_err)",
+    )
+
+    # δn^{(1)}_F(kσ) = f'(ϵₖ) (Σ^λ_F(k) - Σ^λ_F(k_F)) = f'(ϵₖ) (Σ^λ_F(k) + δμ₁)
+    # δn^{(2)}_F(kσ) = f''(ϵₖ) (Σ^λ_F(k) - Σ^λ_F(k_F))^2 = f''(ϵₖ) (Σ^λ_F(k) + δμ₁)^2
+    dn1F_exact_fine = @. fpe_fine * (sigma_lambda_fock_exact(kgrid_fine, [param]) + δμ1)
+    dn2F_exact_fine = @. fppe_fine * (sigma_lambda_fock_exact(kgrid_fine, [param]) + δμ1)^2
+    dn3F_exact_fine = @. fpppe_fine * (sigma_lambda_fock_exact(kgrid_fine, [param]) + δμ1)^3
+
+    println("f''(0): $(Spectral.kernelFermiT_dω2.(-1e-8, 0.0, param.β))")
+
+    # Mean and error bars for the computed results
+    dn2F_calc = data[(2, 0, 0)] + δμ[1] * data[(1, 1, 0)] + δμ[1]^2 * data[(0, 2, 0)]
+    dn2F_calc_exact_mu = data[(2, 0, 0)] + δμ1 * data[(1, 1, 0)] + δμ1^2 * data[(0, 2, 0)]
+    dn2F_means = Measurements.value.(dn2F_calc)
+    dn2F_stdevs = Measurements.uncertainty.(dn2F_calc)
+    dn2F_means_exact_mu = Measurements.value.(dn2F_calc_exact_mu)
+    dn2F_stdevs_exact_mu = Measurements.uncertainty.(dn2F_calc_exact_mu)
+
+    # Get ratio of exact to calculated result on coarse kgrid
+    ϵk = @. kgrid^2 / (2 * param.me) - param.μ
+    # fppe = -Spectral.kernelFermiT_dω2.(-1e-8, ϵk, param.β)
+    fppe = @. (param.β^2 / 4) * sech(param.β * ϵk / 2)^2 * tanh.(param.β * ϵk / 2)
+    dn2F_exact = @. fppe * (sigma_lambda_fock_exact(kgrid, [param]) + δμ1)^2
+    ratio = dn2F_exact ./ dn2F_means_exact_mu
+    max_ratio = argmax(abs, ratio)
+    println(ratio)
+    println(max_ratio)
+
+    # Plot the Fock series contribution to the second-order occupation shift
+    fig, ax = plt.subplots()
+    ax.axhline(0.0; linestyle="--", linewidth=1, color="gray")
+    # Exact result
+    ax.plot(
+        k_kf_grid_fine,
+        dn2F_exact_fine,
+        "k";
+        label="\$f^{\\prime\\prime}(\\xi_k) \\left(\\Sigma^\\lambda_F(k) - \\Sigma^\\lambda_F(k_F)\\right)^2\$ (exact)",
+    )
+    # ax.plot(
+    #     k_kf_grid_fine,
+    #     dn3F_exact_fine,
+    #     "r";
+    #     label="\$\\delta n^3_F(k) = f^{\\prime\\prime\\prime}(\\epsilon_k) \\left(\\Sigma^\\lambda_F(k) - \\Sigma^\\lambda_F(k_F)\\right)^3\$ (exact)",
+    # )
+    # Plot mean and error bar for the computed result
+    marker = "o-"
+    ax.plot(
+        k_kf_grid,
+        # dn2F_means,
+        dn2F_means_exact_mu,
+        marker;
+        markersize=2,
+        color="r",
+        label="\$\\delta n^{(2)}_{F}(k)\$ ($solver)",
+    )
+    ax.fill_between(
+        k_kf_grid,
+        # dn2F_means - dn2F_stdevs,
+        # dn2F_means + dn2F_stdevs;
+        dn2F_means_exact_mu - dn2F_stdevs_exact_mu,
+        dn2F_means_exact_mu + dn2F_stdevs_exact_mu;
+        color="r",
+        alpha=0.4,
+    )
+    ax.legend(; loc="best")
+    ax.set_xlim(0.75, 1.25)
+    ax.set_ylim(-20, 27)
+    ax.set_xlabel("\$k / k_F\$")
+    ax.set_ylabel("\$\\delta n^{(2)}_F({k,\\sigma})\$")
+    xloc = 1.025
+    yloc = -8
+    ydiv = -5
+    ax.text(
+        xloc,
+        yloc,
+        "\$r_s = 1,\\, \\beta \\hspace{0.1em} \\epsilon_F = $(beta),\$";
+        fontsize=14,
+    )
+    ax.text(
+        xloc,
+        yloc + ydiv,
+        "\$\\lambda = $(mass2)\\epsilon_{\\mathrm{Ry}},\\, N_{\\mathrm{eval}} = \\mathrm{$(neval)}\$";
+        fontsize=14,
+    )
+    fig.tight_layout()
+    fig.savefig(
+        "results/occupation/occupation_shift_n=2_fock_rs=$(param.rs)_" *
+        "beta_ef=$(param.beta)_lambda=$(param.mass2)_neval=$(neval)_$(solver).pdf",
+    )
+
+    dn0F_total = fe_fine
+    dn1F_total = dn0F_total + dn1F_exact_fine
+    dn2F_total = dn1F_total + dn2F_exact_fine
+    dn3F_total = dn2F_total + dn3F_exact_fine
+
+    # # Plot the occupation shift to second order in the Fock series
+    # fig, ax = plt.subplots()
+    # ax.axhspan(0, 1; alpha=0.2, facecolor="k", edgecolor=nothing)
+    # ax.axvline(1.0; linestyle="--", linewidth=1, color="gray")
+    # # Exact results
+    # ax.plot(k_kf_grid_fine, dn0F_total; label="\$N=0\$")
+    # # Add back exact screened Fock series contribution to full N=0 result
+    # ax.plot(k_kf_grid_fine, dn2F_exact_fine + dn0F_total; label="\$N=0\$ plus \$n=2\$")
+    # ax.legend(; loc="best")
+    # ax.set_xlim(0.75, 1.25)
+    # ax.set_ylim(-19, 19)
+    # ax.set_xlabel("\$k / k_F\$")
+    # ax.set_ylabel(
+    #     "\$n_N(k,\\sigma)\$",
+    # )
+    # xloc = 1.03
+    # yloc = -8.5
+    # ydiv = -6
+    # ax.text(
+    #     xloc,
+    #     yloc,
+    #     "\$r_s = 1,\\, \\beta \\hspace{0.1em} \\epsilon_F = $(beta), \\lambda = $(mass2)\\epsilon_{\\mathrm{Ry}}\$";
+    #     fontsize=12,
+    # )
+    # fig.tight_layout()
+    # fig.savefig("results/occupation/occupation_N=1_plus_N=2_fock.pdf")
+
+    # Plot the occupation Fock insertion series
+    fig, ax = plt.subplots()
+    ax.axhspan(0, 1; alpha=0.2, facecolor="k", edgecolor=nothing)
+    ax.axvline(1.0; linestyle="--", linewidth=1, color="gray")
+    # Exact results
+    ax.plot(k_kf_grid_fine, dn0F_total; label="\$N=0\$")
+    ax.plot(k_kf_grid_fine, dn1F_total; label="\$N=1\$")
+    ax.plot(k_kf_grid_fine, dn2F_total; label="\$N=2\$")
+    # ax.plot(
+    #     k_kf_grid_fine,
+    #     dn3F_total;
+    #     label="\$N=3\$",
+    # )
+    ax.legend(; loc="best")
+    ax.set_xlim(0.75, 1.25)
+    ax.set_ylim(-17, 22)
+    ax.set_xlabel("\$k / k_F\$")
+    ax.set_ylabel(
+        "\$\\sum^N_{n=0} f^{(n)}(\\xi_k) (\\Sigma^\\lambda_F(k) - \\Sigma^\\lambda_F(k_F))^n \$",
+    )
+    xloc = 1.03
+    yloc = -8.5
+    ydiv = -6
+    ax.text(
+        xloc,
+        yloc,
+        "\$r_s = 1,\\, \\beta \\hspace{0.1em} \\epsilon_F = $(beta), \\lambda = $(mass2)\\epsilon_{\\mathrm{Ry}}\$";
+        fontsize=12,
+    )
+    fig.tight_layout()
+    fig.savefig(
+        "results/occupation/occupation_shift_N=2_fock_exact_rs=$(param.rs)_" *
+        "beta_ef=$(param.beta)_lambda=$(param.mass2)_neval=$(neval)_$(solver).pdf",
+    )
+
+    plt.close("all")
+    return
+end
+
+main()
