f7657fd9279d2e8d37531791e12e760880e13e73
dcerkoney <dcerkoney@gmail.com>
2023-02-28 08:34:28 -0800

    Update results
    Former-commit-id: e49710fcd3d6a2187ac6fd295366be4762508608

--- examples/occupation/integrate_occupation.jl
+++ examples/occupation/integrate_occupation.jl
@@ -238,7 +238,7 @@ function main()
     solver = :vegasmc
 
     # Number of evals below and above kF
-    neval = 1e8
+    neval = 1e10
 
     # Enable/disable interaction and chemical potential counterterms
     renorm_mu = true
