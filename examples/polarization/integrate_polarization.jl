e05e7cf2c5b4adda52c4def34846c30750266e0c
dcerkoney <dcerkoney@gmail.com>
2023-03-16 00:14:24 -0400

    Add polarization script
    Former-commit-id: cdd14a2d38f40b2ed274a86176eba6e8ff1f28bb

---
+++ examples/polarization/integrate_polarization.jl
@@ -0,0 +1,343 @@
+using AbstractTrees
+using CodecZlib
+using CompositeGrids
+using ElectronGas
+using ElectronLiquid
+using ElectronLiquid.UEG
+using FeynmanDiagram
+using JLD2
+using Measurements
+using MCIntegration
+using Lehmann
+using LinearAlgebra
+using Parameters
+using PyCall
+using SOSEM
+
+# For saving/loading numpy data
+@pyimport numpy as np
+
+function build_polarization_with_ct(
+    orders;
+    renorm_mu=true,
+    renorm_lambda=true,
+    isFock=false,
+)
+    DiagTree.uidreset()
+    valid_partitions = Vector{PartitionType}()
+    diagparams = Vector{DiagParaF64}()
+    diagtrees = Vector{DiagramF64}()
+    exprtrees = Vector{ExprTreeF64}()
+    # Build all counterterm partitions at the given orders (lowest order (Π₀) is N = 1)
+    partitions = DiagGen.counterterm_partitions(
+        orders;
+        n_lowest=1,
+        renorm_mu=renorm_mu,
+        renorm_lambda=renorm_lambda,
+    )
+    @debug "Partitions: $partitions"
+    for p in partitions
+        # Build diagram tree for this partition
+        @debug "Partition (n_loop, n_ct_mu, n_ct_lambda): $p"
+        diagparam, diagtree = build_diagtree(; n_loop=p[1])
+        # Build tree with counterterms (∂λ(∂μ(DT))) via automatic differentiation
+        dμ_diagtree = DiagTree.derivative([diagtree], BareGreenId, p[2]; index=1)
+        dλ_dμ_diagtree = DiagTree.derivative(dμ_diagtree, BareInteractionId, p[3]; index=2)
+        if isempty(dλ_dμ_diagtree)
+            @warn("Ignoring partition $p with no diagrams")
+        else
+            isFock && DiagTree.removeHartreeFock!(dλ_dμ_diagtree)
+            @debug "\nDiagTree:\n" * repr_tree(dλ_dμ_diagtree)
+            # Compile to expression tree and save results for this partition
+            exprtree = ExprTree.build(dλ_dμ_diagtree)
+            push!(valid_partitions, p)
+            push!(diagparams, diagparam)
+            push!(exprtrees, exprtree)
+            append!(diagtrees, dλ_dμ_diagtree)
+        end
+    end
+    return valid_partitions, diagparams, diagtrees, exprtrees
+end
+
+function build_diagtree(; n_loop=0)
+    # Polarization diagram parameters
+    #
+    # NOTE: Differentiation and Fock filter do not commute, so we need to manually zero
+    #       out the Fock insertions after differentiation (via DiagTree.removeHartreeFock!)
+    DiagTree.uidreset()
+    diagparam = DiagParaF64(;
+        type=PolarDiag,
+        hasTau=true,
+        innerLoopNum=n_loop,
+        totalTauNum=n_loop + 2,
+        filter=[Proper, NoHartree],
+        interaction=[FeynmanDiagram.Interaction(ChargeCharge, Instant)],  # Yukawa interaction
+    )
+    # Build diagram tree dataframe
+    diag_df = Parquet.polarization(diagparam; name=:Π)
+    # Merge spins, Π_σ = Π↑↑ + Π↑↓
+    diagtree = mergeby(diag_df.diagram)[1]
+    @debug "\nDiagTree:\n" * repr_tree(diagtree)
+    return diagparam, diagtree
+end
+
+function integrate_polarization_with_ct(
+    mcparam::UEG.ParaMC,
+    diagparams::Vector{DiagParaF64},
+    exprtrees::Vector{ExprTreeF64};
+    kgrid=[0.0],
+    tgrid=[mcparam.β - 1e-8],
+    alpha=3.0,
+    neval=1e5,
+    print=-1,
+    solver=:vegasmc,
+)
+    @assert all(p.totalTauNum ≤ mcparam.order + 2 for p in diagparams)
+
+    # We assume that each partition expression tree has a single root
+    @assert all(length(et.root) == 1 for et in exprtrees)
+
+    # List of expression tree roots, external times, and inner
+    # loop numbers for each tree (to be passed to integrand)
+    # roots = [et.root[1] for et in exprtrees]
+    innerLoopNums = [p.innerLoopNum for p in diagparams]
+
+    # Grid sizes
+    n_kgrid = length(kgrid)
+    n_tgrid = length(tgrid)
+
+    # Temporary array for combined K-variables [ExtK, K].
+    # We use the maximum necessary loop basis size for K pool.
+    maxloops = maximum(p.totalLoopNum for p in diagparams)
+    varK = zeros(3, maxloops)
+
+    # Build adaptable MC integration variables
+    (K, T, ExtKidx, ExtTidx) = polarization_mc_variables(mcparam, n_kgrid, n_tgrid, alpha)
+
+    # MC configuration degrees of freedom (DOF): shape(K), shape(T), shape(ExtKidx)
+    # We do not integrate the external times and Π is dynamic, hence n_τ = totalTauNum - 2
+    dof = [[p.innerLoopNum, p.totalTauNum - 2, 1, 1] for p in diagparams]
+    println("Integration DOF: $dof")
+
+    # UEG SOSEM diagram observables are a function of |k| only (equal-time)
+    obs = repeat([zeros(n_kgrid, n_tgrid)], length(dof))  # observable for each partition
+
+    # External times are fixed for left/right measurement of the discontinuity at τ = 0
+    T.data[1] = 0  # τin = 0 (COM coordinates)
+
+    # Phase-space factors
+    phase_factors = [1.0 / (2π)^(mcparam.dim * nl) for nl in innerLoopNums]
+
+    # Total prefactors (including outer spin sum factor S=2)
+    prefactors = mcparam.spin * phase_factors
+
+    return integrate(
+        integrand;
+        solver=solver,
+        measure=measure,
+        neval=neval,
+        print=print,
+        # MC config kwargs
+        userdata=(mcparam, exprtrees, innerLoopNums, prefactors, varK, kgrid, tgrid),
+        var=(K, T, ExtKidx, ExtTidx),
+        dof=dof,
+        obs=obs,
+    )
+end
+
+"""Build variable pools for the polarization."""
+function polarization_mc_variables(
+    mcparam::UEG.ParaMC,
+    n_kgrid::Int,
+    n_tgrid::Int,
+    alpha::Float64,
+)
+    R = Continuous(0.0, 1.0; alpha=alpha)
+    Theta = Continuous(0.0, 1π; alpha=alpha)
+    Phi = Continuous(0.0, 2π; alpha=alpha)
+    K = CompositeVar(R, Theta, Phi)
+    # Offset T pool by 2 for fixed/binned external times τin/τout
+    T = Continuous(0.0, mcparam.β; offset=2, alpha=alpha)
+    # Bin in external momentum & time
+    ExtKidx = Discrete(1, n_kgrid; alpha=alpha)
+    ExtTidx = Discrete(1, n_tgrid; alpha=alpha)
+    return (K, T, ExtKidx, ExtTidx)
+end
+
+"""Measurement for multiple diagram trees (counterterm partitions)."""
+function measure(vars, obs, weights, config)
+    ik = vars[3][1]  # ExtK bin index
+    it = vars[4][1]  # ExtT bin index
+    # Measure the weight of each partition
+    for o in 1:(config.N)
+        obs[o][ik, it] += weights[o]
+    end
+    return
+end
+
+"""Integrand for the polarization Π."""
+function integrand(vars, config)
+    # We sample internal momentum/times, and external momentum index
+    K, varT, ExtKidx, ExtTidx = vars
+    R, Theta, Phi = K
+
+    # Unpack userdata
+    mcparam, exprtrees, innerLoopNums, prefactors, varK, kgrid, tgrid = config.userdata
+
+    # External momentum via random index into kgrid (wlog, we place it along the x-axis)
+    ik = ExtKidx[1]
+    varK[1, 1] = kgrid[ik]
+
+    # External time via random index into tgrid
+    it = ExtTidx[1]
+    varT.data[2] = tgrid[it]
+
+    # Evaluate the integrand for each partition
+    integrand = Vector(undef, config.N)
+    for i in 1:(config.N)
+        phifactor = 1.0
+        for j in 1:innerLoopNums[i]  # config.dof[i][1]
+            r = R[j] / (1 - R[j])
+            θ = Theta[j]
+            ϕ = Phi[j]
+            varK[1, j + 1] = r * sin(θ) * cos(ϕ)
+            varK[2, j + 1] = r * sin(θ) * sin(ϕ)
+            varK[3, j + 1] = r * cos(θ)
+            phifactor *= r^2 * sin(θ) / (1 - R[j])^2
+        end
+        # @assert T.data[1] == 0
+
+        @debug "K = $(varK)" maxlog = 3
+        @debug "ik = $ik" maxlog = 3
+        @debug "ExtK = $(kgrid[ik])" maxlog = 3
+
+        # Evaluate the expression tree (additional = mcparam)
+        ExprTree.evalKT!(exprtrees[i], varK, varT.data, mcparam)
+
+        # Evaluate the polarization integrand Π for this partition
+        root = exprtrees[i].root[1]  # there is only one root per partition
+        weight = exprtrees[i].node.current
+        integrand[i] = phifactor * prefactors[i] * weight[root]
+    end
+    return integrand
+end
+
+"""MC integration of the polarization."""
+function main()
+    # Change to project directory
+    if haskey(ENV, "SOSEM_CEPH")
+        cd(ENV["SOSEM_CEPH"])
+    elseif haskey(ENV, "SOSEM_HOME")
+        cd(ENV["SOSEM_HOME"])
+    end
+
+    # Debug mode
+    if isinteractive()
+        ENV["JULIA_DEBUG"] = Main
+    end
+
+    # Total loop order N
+    orders = [1, 2]
+    max_order = maximum(orders)
+    sort!(orders)
+
+    # Settings
+    alpha = 3.0
+    print = 0
+    solver = :vegasmc
+
+    # Number of evals below and above kF
+    neval = 5e10
+
+    # Enable/disable interaction and chemical potential counterterms
+    renorm_mu = true
+    renorm_lambda = true
+
+    # Remove Fock insertions?
+    isFock = false
+
+    # UEG parameters for MC integration
+    param = ParaMC(;
+        order=max_order,
+        rs=1.0,
+        beta=40.0,
+        mass2=1.0,
+        isDynamic=false,
+        isFock=isFock,  # remove Fock insertions
+    )
+    @debug "β * EF = $(param.beta), β = $(param.β), EF = $(param.EF)"
+
+    # K-mesh for measurement
+    minK = 0.2 * param.kF
+    Nk, korder = 4, 7
+    kgrid =
+        CompositeGrid.LogDensedGrid(
+            :uniform,
+            [0.0, 3 * param.kF],
+            [2 * param.kF],
+            Nk,
+            minK,
+            korder,
+        ).grid
+
+    # Dimensionless k-grid
+    # k_kf_grid = kgrid / param.kF
+
+    # T-mesh for measurement; we need a fine τ-grid for accurate integration
+    n_tau = 1000
+    tgrid = collect(LinRange(1e-8, param.β - 1e-8, n_tau))
+
+    # Build diagram/expression trees for the polarization to order
+    # ξᴺ in the renormalized perturbation theory (includes CTs in μ and λ)
+    partitions, diagparams, diagtrees, exprtrees = build_polarization_with_ct(
+        orders;
+        renorm_mu=renorm_mu,
+        renorm_lambda=renorm_lambda,
+        isFock=isFock,
+    )
+
+    println("Integrating partitions: $partitions")
+    println("diagtrees: $diagtrees")
+    println("exprtrees: $exprtrees")
+
+    res = integrate_polarization_with_ct(
+        param,
+        diagparams,
+        exprtrees;
+        kgrid=kgrid,
+        tgrid=tgrid,
+        alpha=alpha,
+        neval=neval,
+        print=print,
+        solver=solver,
+    )
+
+    # Distinguish results with different counterterm schemes
+    ct_string = (renorm_mu || renorm_lambda) ? "_with_ct" : ""
+    if renorm_mu
+        ct_string *= "_mu"
+    end
+    if renorm_lambda
+        ct_string *= "_lambda"
+    end
+    if isFock
+        ct_string *= "_noFock"
+    end
+
+    # Save to JLD2 on main thread
+    if !isnothing(res)
+        savename =
+            "results/data/polarization_n=$(param.order)_rs=$(param.rs)_beta_ef=$(param.beta)_" *
+            "lambda=$(param.mass2)_neval=$(neval)_$(solver)$(ct_string)"
+        jldopen("$savename.jld2", "a+"; compress=true) do f
+            key = "$(UEG.short(param))"
+            if haskey(f, key)
+                @warn("replacing existing data for $key")
+                delete!(f, key)
+            end
+            return f[key] = (orders, param, kgrid, partitions, res)
+        end
+    end
+end
+
+main()
---
+++ examples/polarization/plot_polarization_total.jl
@@ -0,0 +1,397 @@
+using CodecZlib
+using DataFrames
+using DelimitedFiles
+using ElectronGas
+using ElectronLiquid
+using Interpolations
+using JLD2
+using Lehmann
+using LsqFit
+using Measurements
+using Parameters
+using Polynomials
+using PyCall
+using SOSEM
+
+# For saving/loading numpy data
+@pyimport numpy as np
+@pyimport matplotlib.pyplot as plt
+@pyimport mpl_toolkits.axes_grid1.inset_locator as il
+
+function main()
+    @todo  # continue refactoring
+
+    # Change to project directory
+    if haskey(ENV, "SOSEM_CEPH")
+        cd(ENV["SOSEM_CEPH"])
+    elseif haskey(ENV, "SOSEM_HOME")
+        cd(ENV["SOSEM_HOME"])
+    end
+
+    rs = 1.0
+    beta = 40.0
+    mass2 = 1.0
+    solver = :vegasmc
+
+    # Number of evals
+    neval = 5e10
+
+    # Plot total results for orders min_order_plot ≤ ξ ≤ max_order_plot
+    min_order = 1
+    max_order = 2
+    min_order_plot = 1
+    max_order_plot = 2
+
+    # Save total results
+    save = true
+
+    # Distinguish results with fixed vs re-expanded bare interactions
+    intn_str = ""
+
+    # Enable/disable interaction and chemical potential counterterms
+    renorm_mu = true
+    renorm_lambda = true
+
+    # Remove Fock insertions?
+    isFock = false
+
+    # Ignore measured mu/lambda partitions?
+    fix_mu = false
+    fix_lambda = false
+    fix_string = fix_mu || fix_lambda ? "_fix" : ""
+    if fix_mu
+        fix_string *= "_mu"
+    end
+    if fix_lambda
+        fix_string *= "_lambda"
+    end
+
+    # Distinguish results with different counterterm schemes
+    ct_string = (renorm_mu || renorm_lambda) ? "with_ct" : ""
+    if renorm_mu
+        ct_string *= "_mu"
+    end
+    if renorm_lambda
+        ct_string *= "_lambda"
+    end
+    ct_string_short = ct_string
+    if isFock
+        ct_string *= "_noFock"
+    end
+
+    # UEG parameters for MC integration
+    loadparam = ParaMC(;
+        order=max_order,
+        rs=rs,
+        beta=beta,
+        mass2=mass2,
+        isDynamic=false,
+        isFock=isFock,
+    )
+
+    # Load the raw data
+    savename =
+        "results/data/polarization_n=$(max_order)_rs=$(rs)_beta_ef=$(beta)_" *
+        "lambda=$(mass2)_neval=$(neval)_$(solver)$(ct_string)"
+    orders, param, kgrid, partitions, res = jldopen("$savename.jld2", "a+") do f
+        key = "$(UEG.short(loadparam))"
+        return f[key]
+    end
+
+    # Get dimensionless k-grid (k / kF)
+    k_kf_grid = kgrid / param.kF
+    
+    # Get dimensionless t-grid (τ / β)
+    tau_beta_grid = tgrid / param.β
+
+    # Convert results to a Dict of measurements at each order with interaction counterterms merged
+    data = UEG_MC.restodict(res, partitions)
+    for (k, v) in data
+        data[k] = v / (factorial(k[2]) * factorial(k[3]))
+    end
+    # Zero out partitions with mu renorm if present (fix mu)
+    if renorm_mu == false || fix_mu
+        for P in keys(data)
+            if P[2] > 0
+                println("Fixing mu without lambda renorm, ignoring n_k partition $P")
+                data[P] = zero(data[P])
+            end
+        end
+    end
+    # Zero out partitions with lambda renorm if present (fix lambda)
+    if renorm_lambda == false || fix_lambda
+        for P in keys(data)
+            if P[3] > 0
+                println("No lambda renorm, ignoring n_k partition $P")
+                data[P] = zero(data[P])
+            end
+        end
+    end
+
+    println(data)
+    for P in keys(data)
+        # Undo extra minus sign factor
+        data[P] = -data[P]
+    end
+
+    # Zero out double-counted (Fock renormalized) partitions
+    if isFock && min_order ≤ 1
+        data[(1, 0, 0)] = zero(data[(max_order, 0, 0)])
+    end
+
+    merged_data = CounterTerm.mergeInteraction(data)
+
+    # Get exact bare/Fock polarization
+    if param.isFock
+        fock =
+            SelfEnergy.Fock0_ZeroTemp.(kgrid, [param.basic]) .-
+            SelfEnergy.Fock0_ZeroTemp(param.kF, param.basic)
+        ϵk = kgrid .^ 2 / (2 * param.me) .- param.μ + fock  # ϵ_HF = ϵ_0 + (Σ_F(k) - δμ₁)
+    else
+        ϵk = kgrid .^ 2 / (2 * param.me) .- param.μ         # ϵ_0
+    end
+    bare_occupation_exact = -Spectral.kernelFermiT.(-1e-8, ϵk, param.β)
+
+    # Set bare result manually using exact Fermi function
+    if haskey(merged_data, (0, 0)) == false
+        # treat quadrature data as numerically exact
+        merged_data[(0, 0)] = [measurement(bare_occupation_exact, 0.0)]
+    end
+    if min_order == 0
+        stdscores = stdscore.(merged_data[(0, 0)], bare_occupation_exact)
+        worst_score = argmax(abs, stdscores)
+        println("Worst standard score for N=0 (measured): $worst_score")
+        # println("Scores:\n$stdscores")
+        if isFock
+            bm_occupation_2_meas = measurement.(bm_occupation_0, bm_occupation_0_err)
+            stdscores = stdscore.(bm_occupation_2_meas, bare_occupation_exact)
+            worst_score = argmax(abs, stdscores)
+            println("Worst standard score for N=0 (benchmark): $worst_score")
+            # println("Scores:\n$stdscores")
+            @assert worst_score ≤ 10
+        end
+    end
+
+    # Reexpand merged data in powers of μ
+    ct_filename = "examples/counterterms/data_Z_$(ct_string_short).jld2"
+    z, μ = UEG_MC.load_z_mu(param; ct_filename=ct_filename)
+    # Add Taylor factors to CT data
+    for (p, v) in z
+        z[p] = v / (factorial(p[2]) * factorial(p[3]))
+    end
+    for (p, v) in μ
+        μ[p] = v / (factorial(p[2]) * factorial(p[3]))
+    end
+    # Zero out partitions with mu renorm if present (fix mu)
+    if renorm_mu == false || fix_mu
+        for P in keys(μ)
+            if P[2] > 0
+                println("Fixing mu without lambda renorm, ignoring μ partition $P")
+                μ[P] = zero(μ[P])
+            end
+        end
+    end
+    # Zero out partitions with lambda renorm if present (fix lambda)
+    if renorm_lambda == false || fix_lambda
+        for P in keys(μ)
+            if P[3] > 0
+                println("No lambda renorm, ignoring μ partition $P")
+                μ[P] = zero(μ[P])
+            end
+        end
+    end
+    δz, δμ = CounterTerm.sigmaCT(max_order, μ, z; isfock=isFock, verbose=1)
+
+    println("Computed δμ: ", δμ)
+    polarization = UEG_MC.chemicalpotential_renormalization_green(
+        merged_data,
+        δμ;
+        min_order=0,
+        max_order=max_order,
+    )
+    occupation_2_manual =
+        merged_data[(2, 0)] +
+        δμ[1] * merged_data[(1, 1)] +
+        δμ[1]^2 * merged_data[(0, 2)] +
+        δμ[2] * merged_data[(0, 1)]
+    scores_2 = stdscore.(polarization[2] - occupation_2_manual, 0.0)
+    println("Deviation: ", polarization[2] - occupation_2_manual)
+    worst_score_2 = argmax(abs, scores_2)
+    println("2nd order renorm vs manual worst score: $worst_score_2")
+
+    if max_order ≥ 1 && renorm_mu == true && isFock == false && fix_mu == false
+        # Test manual renormalization with exact lowest-order chemical potential
+        δμ1_exact = UEG_MC.delta_mu1(param)  # = ReΣ₁[λ](kF, 0)
+        # nₖ⁽¹⁾ = nₖ_{1,0} + δμ₁ nₖ_{0,1}
+        occupation_1_manual = merged_data[(1, 0)] + δμ1_exact * merged_data[(0, 1)]
+        stdscores = stdscore.(polarization[1], occupation_1_manual)
+        worst_score = argmax(abs, stdscores)
+        println("Exact δμ₁: ", δμ1_exact)
+        println("Computed δμ₁: ", δμ[1])
+        println(
+            "Worst standard score for total result to 1st " *
+            "order (auto vs exact+manual): $worst_score",
+        )
+        @assert worst_score ≤ 10
+    end
+
+    println(UEG.paraid(param))
+    println(partitions)
+    println(res)
+
+    if save
+        savename =
+            "results/data/rs=$(param.rs)_beta_ef=$(param.beta)_" *
+            "lambda=$(param.mass2)_$(intn_str)$(solver)_$(ct_string)"
+        f = jldopen("$savename.jld2", "a+"; compress=true)
+        # NOTE: no bare result for c1b observable (accounted for in c1b0)
+        for N in min_order_plot:max_order
+            num_eval = N == 3 ? neval3 : neval12
+            # Skip exact (N = 0) result
+            N == 0 && continue
+            # Skip Fock result if HF renormalization was used
+            isFock && N == 1 && continue
+            # Update existing results if applicable
+            if haskey(f, "polarization") &&
+               haskey(f["polarization"], "N=$N") &&
+               haskey(f["polarization/N=$N"], "neval=$num_eval")
+                @warn("replacing existing data for N=$N, neval=$num_eval")
+                delete!(f["polarization/N=$N"], "neval=$num_eval")
+            end
+            f["polarization/N=$N/neval=$num_eval/meas"] = polarization_total[N]
+            f["polarization/N=$N/neval=$num_eval/param"] = param
+            f["polarization/N=$N/neval=$num_eval/kgrid"] = kgrid
+        end
+    end
+
+    # Use LaTex fonts for plots
+    plt.rc("text"; usetex=true)
+    plt.rc("font"; family="serif")
+
+    kgrid_fine = param.kF * np.linspace(0.0, 3.0; num=600)
+    k_kf_grid_fine = np.linspace(0.0, 3.0; num=600)
+
+    # Plot the polarization number for each aggregate order
+    fig, ax = plt.subplots()
+    ax.axvline(1.0; linestyle="--", linewidth=1, color="gray")
+    if min_order_plot == 0
+        # Include bare polarization fₖ in plot
+        ax.plot(k_kf_grid_fine, bare_occupation_fine, "k"; label="\$N=0\$ (exact)")
+    end
+    ic = 1
+    colors = ["C0", "C1", "C2", "C3"]
+    # colors = ["orchid", "cornflowerblue", "turquoise", "chartreuse"]
+    for (i, n) in enumerate(min_order:max_order_plot)
+        # n == 0 && continue
+        # Plot measured data
+        means = Measurements.value.(polarization_total[n])
+        stdevs = Measurements.uncertainty.(polarization_total[n])
+        marker = "o-"
+        ax.plot(
+            k_kf_grid,
+            means,
+            marker;
+            markersize=2,
+            color="$(colors[ic])",
+            label="\$n=$n\$ ($solver)",
+        )
+        ax.fill_between(
+            k_kf_grid,
+            means - stdevs,
+            means + stdevs;
+            color="$(colors[ic])",
+            alpha=0.4,
+        )
+        ic += 1
+    end
+    ax.legend(; loc="best")
+    # ax.set_xlim(minimum(k_kf_grid), maximum(k_kf_grid))
+    # ax.set_xlim(0.75, 1.25)
+    ax.set_xlim(0, 3)
+    # ax.set_ylim(nothing, 2)
+    ax.set_xlabel("\$k / k_F\$")
+    ax.set_ylabel("\$\\Pi_n(q, \\tau)\$")
+    if isFock
+        xloc = 1.035
+        yloc = 0.6
+        ydiv = -0.125
+    else
+        # xloc = 1.5
+        xloc = 1.125
+        yloc = 0.5
+        ydiv = -0.15
+    end
+    ax.text(
+        xloc,
+        yloc,
+        "\$r_s = 1,\\, \\beta \\hspace{0.1em} \\epsilon_F = $(beta),\$";
+        fontsize=14,
+    )
+    ax.text(
+        xloc,
+        yloc + ydiv,
+        "\$\\lambda = $(mass2)\\epsilon_{\\mathrm{Ry}},\\, N_{\\mathrm{eval}} = \\mathrm{$(neval)}\$";
+        # "\$\\lambda = \\frac{\\epsilon_{\\mathrm{Ry}}}{10},\\, N_{\\mathrm{eval}} = \\mathrm{$(neval)},\$";
+        fontsize=14,
+    )
+    fig.tight_layout()
+    if inset
+        # Reset color index
+        ic = 1
+        # Plot inset
+        ax_inset =
+            il.inset_axes(ax; width="38%", height="28.5%", loc="lower left", borderpad=0)
+        # Compare result to bare polarization fₖ
+        ax_inset.plot(k_kf_grid_fine, bare_occupation_fine, "k"; label="\$N=0\$ (exact)")
+        ax_inset.axvline(1.0; linestyle="--", linewidth=1, color="gray")
+        # ax_inset.axhspan(0, 1; alpha=0.2, facecolor="k", edgecolor=nothing)
+        ax_inset.axhline(0.0; linestyle="-", linewidth=0.5, color="k")
+        ax_inset.axhline(1.0; linestyle="-", linewidth=0.5, color="k")
+        for (i, N) in enumerate(min_order:max_order_plot)
+            # for (i, N) in enumerate(min_order:1)
+            # N == 0 && continue
+            isFock && N == 1 && continue
+            # Get means and error bars from the result up to this order
+            means = Measurements.value.(polarization_total[N])
+            stdevs = Measurements.uncertainty.(polarization_total[N])
+            marker = "o-"
+            ax_inset.plot(
+                k_kf_grid,
+                means,
+                marker;
+                markersize=2,
+                color="$(colors[ic])",
+                label="\$N=$N\$ ($solver)",
+            )
+            ax_inset.fill_between(
+                k_kf_grid,
+                means - stdevs,
+                means + stdevs;
+                color="$(colors[ic])",
+                alpha=0.4,
+            )
+            ic += 1
+        end
+        xpad = 0.04
+        ypad = 0.2
+        ax_inset.set_xlim(0.95 - xpad, 1.05 + xpad)
+        ax_inset.set_ylim(-ypad, 1 + ypad)
+        ax_inset.set_xlabel("\$k / k_F\$"; labelpad=7)
+        ax_inset.set_ylabel("\$n_{k\\sigma}\$")
+        ax_inset.xaxis.set_label_position("top")
+        ax_inset.yaxis.set_label_position("right")
+        ax_inset.xaxis.tick_top()
+        ax_inset.yaxis.tick_right()
+    end
+    dirstring = isFock ? "results/polarization/benchmark/" : "results/polarization/"
+    fig.savefig(
+        dirstring *
+        "occupation_N=$(max_order_plot)_rs=$(param.rs)_" *
+        "beta_ef=$(param.beta)_lambda=$(param.mass2)_neval=$(neval)_$(solver)_$(ct_string)$(fix_string).pdf",
+    )
+
+    plt.close("all")
+    return
+end
+
+main()
