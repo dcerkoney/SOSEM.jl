176a3ad876ba56b5d1d4ee62829c962021c12077
dcerkoney <dcerkoney@gmail.com>
2023-03-24 11:54:16 -0700

    Add 3rd order occupation at rs=5
    Former-commit-id: a57dd196e8ac2b235c3079001f8fe7ad6d1a4b67

--- examples/occupation/integrate_occupation.jl
+++ examples/occupation/integrate_occupation.jl
@@ -228,8 +228,8 @@ function main()
     end
 
     # Total loop order N
-    orders = [0, 1, 2]
-    # orders = [3]
+    # orders = [0, 1, 2]
+    orders = [3]
     max_order = maximum(orders)
     sort!(orders)
 
