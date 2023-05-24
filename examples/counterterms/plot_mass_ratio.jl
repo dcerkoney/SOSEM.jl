6cfc36df59c3d883f983f931c8e120d1b5dc7d5b
dcerkoney <dcerkoney@gmail.com>
2023-05-24 17:04:48 -0400

    Update mass results and bugfix for tail fits
    Former-commit-id: 8daed8411ac991086ec2a79417d0ce66fad0006d

--- examples/counterterms/plot_mass_ratio.jl
+++ examples/counterterms/plot_mass_ratio.jl
@@ -71,16 +71,34 @@ function main()
     )
 
     # Load mass ratio data for each idk
-    savename = "mass_ratio_from_sigma"
+    savename_mass = "mass_ratio_from_sigma"
+    savename_zinv = "inverse_zfactor"
+    savename_zapprox = "zfactor_approx"
     local param
     mass_ratios = []
-    print("Loading data from $savename...")
+    print("Loading data from $savename_mass...")
     for idk in idks
-        param, ngrid, kgrid, mass_ratio = jldopen("$savename.jld2", "a+") do f
+        param, ngrid, kgrid, mass_ratio = jldopen("$savename_mass.jld2", "a+") do f
             key = "$(UEG.short(loadparam))_idk=$(idk)"
             return f[key]
         end
         push!(mass_ratios, mass_ratio)
+    end
+    print("Loading data from $savename_zinv...")
+    local zinv
+    for idk in idks
+        param, ngrid, kgrid, zinv = jldopen("$savename_zinv.jld2", "a+") do f
+            key = "$(UEG.short(loadparam))_idk=$(idk)"
+            return f[key]
+        end
+    end
+    print("Loading data from $savename_zapprox...")
+    local zapprox
+    for idk in idks
+        param, ngrid, kgrid, zapprox = jldopen("$savename_zapprox.jld2", "a+") do f
+            key = "$(UEG.short(loadparam))_idk=$(idk)"
+            return f[key]
+        end
     end
     println("done!")
     @assert param.order == max_order
@@ -92,20 +110,20 @@ function main()
     fig, ax = plt.subplots()
     orders = 0:max_order
     for idk in idks
-        means, stdevs =
-            Measurements.value.(mass_ratios[idk]), Measurements.uncertainty.(mass_ratios[idk])
+        means, stdevs = Measurements.value.(mass_ratios[idk]),
+        Measurements.uncertainty.(mass_ratios[idk])
         ax.errorbar(
             orders,
             means,
             stdevs;
             capsize=4,
-            zorder=10*idk,
+            zorder=10 * idk,
             label="\$\\delta K = $(dks[idk]) k_F\$",
         )
     end
-    xloc = 2.0
-    yloc = 1.0
-    ydiv = -0.05
+    xloc = 1.2
+    yloc = 0.85
+    ydiv = -0.03
     ax.text(
         xloc,
         yloc,
@@ -118,7 +136,6 @@ function main()
         "\$\\lambda = $(mass2)\\epsilon_{\\mathrm{Ry}},\\, N_{\\mathrm{eval}} = \\mathrm{$(neval)}\$";
         fontsize=14,
     )
-    ax.set_ylim(0.9, 1.3)
     ax.set_xlabel("\$N\$")
     ax.set_ylabel("\$m^\\star / m\$")
     ax.set_xticks(orders)
@@ -126,7 +143,66 @@ function main()
     ax.legend(; loc="best")
     fig.tight_layout()
     fig.savefig(
-        "effective_mass_ratio_rs=$(param.rs)_beta_ef=$(param.beta)_" *
+        "../../results/effective_mass_ratio/effective_mass_ratio_rs=$(param.rs)_beta_ef=$(param.beta)_" *
+        "lambda=$(param.mass2)_neval=$(neval)_$(solver)_$(ct_string).pdf",
+    )
+
+    fig, ax = plt.subplots()
+    orders = 0:max_order
+    means, stdevs = Measurements.value.(zinv), Measurements.uncertainty.(zinv)
+    ax.errorbar(orders, means, stdevs; capsize=4, zorder=10)
+    xloc = 2.125
+    yloc = 1.03
+    ydiv = -0.01
+    ax.text(
+        xloc,
+        yloc,
+        "\$r_s = $(rs),\\, \\beta \\hspace{0.1em} \\epsilon_F = $(beta),\$";
+        fontsize=14,
+    )
+    ax.text(
+        xloc,
+        yloc + ydiv,
+        "\$\\lambda = $(mass2)\\epsilon_{\\mathrm{Ry}},\\, N_{\\mathrm{eval}} = \\mathrm{$(neval)}\$";
+        fontsize=14,
+    )
+    ax.set_xlabel("\$N\$")
+    ax.set_ylabel("\$Z^{-1}\$")
+    ax.set_xticks(orders)
+    ax.set_xticklabels(orders)
+    fig.tight_layout()
+    fig.savefig(
+        "../../results/effective_mass_ratio/inverse_zfactor_rs=$(param.rs)_beta_ef=$(param.beta)_" *
+        "lambda=$(param.mass2)_neval=$(neval)_$(solver)_$(ct_string).pdf",
+    )
+
+    fig, ax = plt.subplots()
+    orders = 0:max_order
+    means, stdevs =
+        Measurements.value.(zapprox), Measurements.uncertainty.(zapprox)
+    ax.errorbar(orders, means, stdevs; capsize=4, zorder=10)
+    xloc = 2.125
+    yloc = 0.98
+    ydiv = -0.01
+    ax.text(
+        xloc,
+        yloc,
+        "\$r_s = $(rs),\\, \\beta \\hspace{0.1em} \\epsilon_F = $(beta),\$";
+        fontsize=14,
+    )
+    ax.text(
+        xloc,
+        yloc + ydiv,
+        "\$\\lambda = $(mass2)\\epsilon_{\\mathrm{Ry}},\\, N_{\\mathrm{eval}} = \\mathrm{$(neval)}\$";
+        fontsize=14,
+    )
+    ax.set_xlabel("\$N\$")
+    ax.set_ylabel("\$Z_N \\approx 1 / (Z^{-1})_N\$")
+    ax.set_xticks(orders)
+    ax.set_xticklabels(orders)
+    fig.tight_layout()
+    fig.savefig(
+        "../../results/effective_mass_ratio/zfactor_approximation_rs=$(param.rs)_beta_ef=$(param.beta)_" *
         "lambda=$(param.mass2)_neval=$(neval)_$(solver)_$(ct_string).pdf",
     )
     plt.close("all")
