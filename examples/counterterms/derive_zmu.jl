8cd8b6a41ea285a82461c1bf316fe02b18728621
dcerkoney <dcerkoney@gmail.com>
2023-06-23 18:04:22 -0400

    Working on mass, debugging tails
    Former-commit-id: dbfaf4659d19ce7d3079693f61eae62f167ecfbe

--- examples/counterterms/derive_zmu.jl
+++ examples/counterterms/derive_zmu.jl
@@ -21,8 +21,8 @@ end
 # For lambda optimization
 order = [4]  # C^{(1)}_{N≤5} includes CTs up to 4th order
 beta = [40.0]
-rs = [3.0]
-mass2 = [1.5]
+# rs = [3.0]
+# mass2 = [1.5]
 
 # rs = [2.0]
 # mass2 = [1.75]
@@ -32,7 +32,9 @@ mass2 = [1.5]
 # rs = 3.0
 # mass2 = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0, 3.5, 4.0, 5.0, 6.0]
 ### rs = 4 ###
-# rs = 4.0
+rs = [4.0]
+mass2 = [0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0, 1.125]
+# mass2 = [0.25, 0.5, 0.75, 1.0, 1.25]
 # mass2 = [0.25, 0.5, 0.75, 1.0, 1.5, 2.0, 2.25, 2.5, 2.75, 3.0, 3.5, 4.0, 4.5, 5.0, 6.0, 7.0]
 ### rs = 5 ###
 # rs = 5.0
@@ -55,8 +57,8 @@ if renorm_lambda
 end
 
 # const filename = "data/data_Z$(ct_string).jld2"
-const filename = "data/data_Z$(ct_string)_kF.jld2"
-# const filename = "data/data_Z$(ct_string)_kF_opt.jld2"
+# const filename = "data/data_Z$(ct_string)_kF.jld2"
+const filename = "data/data_Z$(ct_string)_kF_opt.jld2"
 const parafilename = "data/para.csv"
 
 """
